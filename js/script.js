406fa9cfaaf942de2230ff5f785658033dc1932c
Juliette Gondon <juliettegondon@gmail.com>
2020-11-19 02:55:30 -0500

    javascript is working

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    var typed = new Typed(".juliette", {
+        strings: ["Juliette Gondon"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+    });
