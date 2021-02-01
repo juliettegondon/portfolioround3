201d9ac6643acb85148088cbdb6f5ac1f8e66834
Juliette Gondon <juliettegondon@gmail.com>
2021-02-01 00:00:42 -0500

    updating removing some pure elements for custom css

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 //typing name out using typed.js
 $(document).ready(function(){
-    var typed = new Typed(".juliette", {
-        strings: ["Juliette Gondon"],
+    var typed = new Typed(".brand-tagline", {
+        strings: ["software"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true,
