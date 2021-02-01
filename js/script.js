d97c5023828134adf1308bed53b32ee7489accb6
Juliette Gondon <juliettegondon@gmail.com>
2021-02-01 17:47:10 -0500

    margins

--- js/script.js
+++ js/script.js
@@ -2,9 +2,29 @@
 $(document).ready(function(){
     var typed = new Typed(".brand-tagline", {
         strings: ["software developer"],
-        typeSpeed: 100,
+        typeSpeed: 40,
         backSpeed: 60,
         loop: true,
         showCursor: false,
     });
     });
+/* 
+     var typed = new Typed("#typed", {
+    stringsElement: '#typed-strings',
+    typeSpeed: 0,
+    backSpeed: 0,
+    backDelay: 500,
+    startDelay: 1000,
+    loop: false,
+    onBegin: function(self) { prettyLog('onBegin ' + self) },
+    onComplete: function(self) { prettyLog('onCmplete ' + self) },
+    preStringTyped: function(pos, self) { prettyLog('preStringTyped ' + pos + ' ' + self); },
+    onStringTyped: function(pos, self) { prettyLog('onStringTyped ' + pos + ' ' + self) },
+    onLastStringBackspaced: function(self) { prettyLog('onLastStringBackspaced ' + self) },
+    onTypingPaused: function(pos, self) { prettyLog('onTypingPaused ' + pos + ' ' + self) },
+    onTypingResumed: function(pos, self) { prettyLog('onTypingResumed ' + pos + ' ' + self) },
+    onReset: function(self) { prettyLog('onReset ' + self) },
+    onStop: function(pos, self) { prettyLog('onStop ' + pos + ' ' + self) },
+    onStart: function(pos, self) { prettyLog('onStart ' + pos + ' ' + self) },
+    onDestroy: function(self) { prettyLog('onDestroy ' + self) }
+  }); */
